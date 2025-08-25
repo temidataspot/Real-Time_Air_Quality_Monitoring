58b242a4d4258dff08f51bfc3665227eb6af2093
Temi Priscilla Jokotola <153321707+temidataspot@users.noreply.github.com>
2025-08-25 14:50:32 +0100

    Update and rename Cleaning, Forecasting, and Analysis in R.md to RScript.R

--- RScript.R
+++ RScript.R
@@ -1,4 +1,4 @@
-# 3: Cleaning, Forecasting, and Analysis in R
+# Cleaning, Forecasting, and Analysis in R
 
 # Objective
 Clean the air quality data and analyze trends, correlations, and outliers using R.
